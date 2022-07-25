c3c77009a45816d5c61d38a97720a20b9b3dab14
Maulik2005 <109951458+Maulik2005@users.noreply.github.com>
2022-07-25 14:44:01 +0530

    Add files via upload

---
+++ maximum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; 
+class Maximum  
+{
+    public static void main(String[] args) 
+    {
+        int a, b, c, d;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter all three numbers:");
+        a = s.nextInt();
+        b = s.nextInt();
+        c = s.nextInt();
+        d = c > (a > b ? a : b) ? c : ((a > b) ? a : b);
+        System.out.println("Maximum Number:"+d);
+    }
+}
+      
+              
